1c077464143831fd62ee34598ccd820f2e5a7095
Lily Tran <tran.lily8@gmail.com>
2022-07-07 13:52:41 -0700

    add initial files

---
+++ BookshelfSwiftUI/BookshelfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfSwiftUIApp.swift
+//  BookshelfSwiftUI
+//
+//  Created by Lily Tran on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BookshelfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
