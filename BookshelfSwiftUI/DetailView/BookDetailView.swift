2e49f186b70011fb46b8fb7f9cd20909bbc91c8b
Lily Tran <tran.lily8@gmail.com>
2022-07-08 14:08:39 -0700

    final product

--- BookshelfSwiftUI/DetailView/BookDetailView.swift
+++ BookshelfSwiftUI/DetailView/BookDetailView.swift
@@ -12,7 +12,58 @@ struct BookDetailView: View {
     @Binding var book: Book
     
     var body: some View {
-        Text(book.author)
+        VStack(spacing: 10) {
+            
+            Image(book.coverArt)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 200)
+                .padding()
+            
+            Text(book.title)
+                .font(.system(.largeTitle))
+                .lineLimit(3)
+            
+            Text(book.author)
+                .foregroundColor(.secondary)
+                .font(.footnote)
+                .lineSpacing(10)
+            
+            HStack {
+                VStack {
+                    Text("Release")
+                    Text(book.releaseYear)
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+                
+                VStack {
+                    Text("Pages")
+                    Text("333")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+                
+                VStack {
+                    Text("Language")
+                    Text("Eng")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+                
+            }
+            .font(.system(size: 14, design: .monospaced))
+            
+            ScrollView {
+                Text(book.description)
+            }
+            .frame(width: UIScreen.screenWidth - 50, height: UIScreen.screenHeight/3)
+            
+            
+            
+            
+        }
+        
     }
 }
 
@@ -25,3 +76,11 @@ struct BookDetailView_Previews: PreviewProvider {
         )
     }
 }
+
+// to get the screen size
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
+
